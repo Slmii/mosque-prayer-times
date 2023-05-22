04994337325504c7ada732752500cae077cb50da
Slmii <selami.c@sbytes-it.com>
2023-05-23 00:07:19 +0200

    fix fetching new times

--- src/lib/context/PrayerTimes.context.tsx
+++ src/lib/context/PrayerTimes.context.tsx
@@ -68,7 +68,7 @@ export const PrayerTimesProvider = ({ children }: PropsWithChildren) => {
 	useEffect(() => {
 		const init = async () => {
 			const prayerTimes = await fetchPrayerTimes();
-			setTimes(prayerTimes);
+			setTimes(prayerTimes, 0);
 		};
 
 		init();
@@ -82,7 +82,7 @@ export const PrayerTimesProvider = ({ children }: PropsWithChildren) => {
 			// Check if it's midnight
 			if (now.getHours() === 0 && now.getMinutes() === 0 && now.getSeconds() === 0) {
 				const prayerTimes = await fetchPrayerTimes();
-				setTimes(prayerTimes);
+				setTimes(prayerTimes, 1);
 			}
 		};
 
@@ -94,25 +94,25 @@ export const PrayerTimesProvider = ({ children }: PropsWithChildren) => {
 		};
 	});
 
-	const setTimes = (prayerTimes: PrayerTimeResponse[]) => {
+	const setTimes = (prayerTimes: PrayerTimeResponse[], index: number) => {
 		setPrayerTimes({
 			today: {
-				Aksam: prayerTimes[0].Aksam,
-				Gunes: prayerTimes[0].Gunes,
-				Ikindi: prayerTimes[0].Ikindi,
-				Imsak: prayerTimes[0].Imsak,
-				Ogle: prayerTimes[0].Ogle,
-				Yatsi: prayerTimes[0].Yatsi,
-				HicriDate: prayerTimes[0].HicriTarihUzun
+				Aksam: prayerTimes[index].Aksam,
+				Gunes: prayerTimes[index].Gunes,
+				Ikindi: prayerTimes[index].Ikindi,
+				Imsak: prayerTimes[index].Imsak,
+				Ogle: prayerTimes[index].Ogle,
+				Yatsi: prayerTimes[index].Yatsi,
+				HicriDate: prayerTimes[index].HicriTarihUzun
 			},
 			tomorrow: {
-				Aksam: prayerTimes[1].Aksam,
-				Gunes: prayerTimes[1].Gunes,
-				Ikindi: prayerTimes[1].Ikindi,
-				Imsak: prayerTimes[1].Imsak,
-				Ogle: prayerTimes[1].Ogle,
-				Yatsi: prayerTimes[1].Yatsi,
-				HicriDate: prayerTimes[1].HicriTarihUzun
+				Aksam: prayerTimes[index + 1].Aksam,
+				Gunes: prayerTimes[index + 1].Gunes,
+				Ikindi: prayerTimes[index + 1].Ikindi,
+				Imsak: prayerTimes[index + 1].Imsak,
+				Ogle: prayerTimes[index + 1].Ogle,
+				Yatsi: prayerTimes[index + 1].Yatsi,
+				HicriDate: prayerTimes[index + 1].HicriTarihUzun
 			}
 		});
 	};
